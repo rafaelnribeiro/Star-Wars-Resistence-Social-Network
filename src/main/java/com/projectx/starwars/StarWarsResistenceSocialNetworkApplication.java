7a8ba23a3183605d197c257db691200d3ee39916
rafaelnribeiro <rafaelnribeiro@outlook.com>
2019-12-11 17:00:37 -0300

    Criando projeto spring

---
+++ src/main/java/com/projectx/starwars/StarWarsResistenceSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.projectx.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsResistenceSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsResistenceSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectx/starwars/StarWarsResistenceSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectx.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsResistenceSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
